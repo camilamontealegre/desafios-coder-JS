c9abc6ad2108a5f742d37c0bad864b56c0b9182f
Camila Montealegre <camilamontealegre7@gmail.com>
2021-11-27 21:53:20 -0500

    conditionals and prompt a message

---
+++ ejercicio_1/conditionals.js
@@ -0,0 +1,6 @@
+let answer = prompt("¿Te gustan los animales? Por favor responde sí o no")
+if( (answer=="si" || answer=="SI" || answer=="sí" || answer=="Sí"|| answer=="Si" ) ){
+    alert("¡Enhorabuena! A nosotros también nos gustan ❤️")
+}else{
+    alert("Acá amamos los animales.🐰 Quizá estés en el sitio equivocado.")
+}
